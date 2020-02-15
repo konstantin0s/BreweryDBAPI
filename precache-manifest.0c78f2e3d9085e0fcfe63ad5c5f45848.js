b44d52c588d555ac6851ff2297dca2829ebebbbb
konstantin0s <constantintofan85@gmail.com>
2020-02-15 20:20:52 +0100

    Updates

---
+++ precache-manifest.0c78f2e3d9085e0fcfe63ad5c5f45848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef4668cbf1796bac9e11025acea1559",
+    "url": "/BreweryDBAPI/beers/index.html"
+  },
+  {
+    "revision": "7bcf5b3ffe30d5c19ae9",
+    "url": "/BreweryDBAPI/beers/static/css/main.3bf1cccd.chunk.css"
+  },
+  {
+    "revision": "7f1345a93dc98b71b581",
+    "url": "/BreweryDBAPI/beers/static/js/2.b15d9d01.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/BreweryDBAPI/beers/static/js/2.b15d9d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bcf5b3ffe30d5c19ae9",
+    "url": "/BreweryDBAPI/beers/static/js/main.4c6b32bd.chunk.js"
+  },
+  {
+    "revision": "225952482a526f46f3d0",
+    "url": "/BreweryDBAPI/beers/static/js/runtime-main.c52f826c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BreweryDBAPI/brewerys/precache-manifest.d9823e5673de30a0ac41099ed98de178.js"
+  "/BreweryDBAPI/beers/precache-manifest.0c78f2e3d9085e0fcfe63ad5c5f45848.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BreweryDBAPI/brewerys/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BreweryDBAPI/beers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
